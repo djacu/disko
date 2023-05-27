a2c13884d1e48073350b76512d493e056f0116fb
Daniel Baker <daniel.baker@tweag.io>
2023-05-26 23:50:39 -0700

    auto-formatting

--- doc.nix
+++ doc.nix
@@ -1,6 +1,10 @@
-{ lib, nixosOptionsDoc, runCommand, fetchurl, pandoc }:
-
-let
+{
+  lib,
+  nixosOptionsDoc,
+  runCommand,
+  fetchurl,
+  pandoc,
+}: let
   diskoLib = import ./lib {
     inherit lib;
     rootMountPoint = "/mnt";
@@ -11,7 +15,7 @@ let
         options.disko = {
           devices = lib.mkOption {
             type = diskoLib.devices;
-            default = { };
+            default = {};
             description = "The devices to set up";
           };
         };
@@ -21,23 +25,25 @@ let
   options = nixosOptionsDoc {
     options = eval.options;
   };
-  md = (runCommand "disko-options.md" { } ''
-    cat >$out <<EOF
-    # Disko options
+  md =
+    (runCommand "disko-options.md" {} ''
+      cat >$out <<EOF
+      # Disko options
 
-    EOF
-    cat ${options.optionsCommonMark} >>$out
-  '').overrideAttrs (o: {
-    # Work around https://github.com/hercules-ci/hercules-ci-agent/issues/168
-    allowSubstitutes = true;
-  });
+      EOF
+      cat ${options.optionsCommonMark} >>$out
+    '')
+    .overrideAttrs (o: {
+      # Work around https://github.com/hercules-ci/hercules-ci-agent/issues/168
+      allowSubstitutes = true;
+    });
   css = fetchurl {
     url = "https://gist.githubusercontent.com/killercup/5917178/raw/40840de5352083adb2693dc742e9f75dbb18650f/pandoc.css";
     sha256 = "sha256-SzSvxBIrylxBF6B/mOImLlZ+GvCfpWNLzGFViLyOeTk=";
   };
 in
-runCommand "disko.html" { nativeBuildInputs = [ pandoc ]; } ''
-  mkdir $out
-  cp ${css} $out/pandoc.css
-  pandoc --css="pandoc.css" ${md} --to=html5 -s -f markdown+smart --metadata pagetitle="Disko options" -o $out/index.html
-''
+  runCommand "disko.html" {nativeBuildInputs = [pandoc];} ''
+    mkdir $out
+    cp ${css} $out/pandoc.css
+    pandoc --css="pandoc.css" ${md} --to=html5 -s -f markdown+smart --metadata pagetitle="Disko options" -o $out/index.html
+  ''
--- lib/types/table.nix
+++ lib/types/table.nix
@@ -1,26 +1,31 @@
-{ config, options, lib, diskoLib, ... }:
 {
+  config,
+  options,
+  lib,
+  diskoLib,
+  ...
+}: {
   options = {
     type = lib.mkOption {
-      type = lib.types.enum [ "table" ];
+      type = lib.types.enum ["table"];
       internal = true;
       description = "Partition table";
     };
     format = lib.mkOption {
-      type = lib.types.enum [ "gpt" "msdos" ];
+      type = lib.types.enum ["gpt" "msdos"];
       default = "gpt";
       description = "The kind of partition table";
     };
     partitions = lib.mkOption {
-      type = lib.types.listOf (lib.types.submodule ({ config, ... }: {
+      type = lib.types.listOf (lib.types.submodule ({config, ...}: {
         options = {
           part-type = lib.mkOption {
-            type = lib.types.enum [ "primary" "logical" "extended" ];
+            type = lib.types.enum ["primary" "logical" "extended"];
             default = "primary";
             description = "Partition type";
           };
           fs-type = lib.mkOption {
-            type = lib.types.nullOr (lib.types.enum [ "btrfs" "ext2" "ext3" "ext4" "fat16" "fat32" "hfs" "hfs+" "linux-swap" "ntfs" "reiserfs" "udf" "xfs" ]);
+            type = lib.types.nullOr (lib.types.enum ["btrfs" "ext2" "ext3" "ext4" "fat16" "fat32" "hfs" "hfs+" "linux-swap" "ntfs" "reiserfs" "udf" "xfs"]);
             default = null;
             description = "Filesystem type to use";
           };
@@ -46,7 +51,7 @@
           };
           flags = lib.mkOption {
             type = lib.types.listOf lib.types.str;
-            default = [ ];
+            default = [];
             description = "Partition flags";
           };
           bootable = lib.mkOption {
@@ -57,7 +62,7 @@
           content = diskoLib.partitionType;
         };
       }));
-      default = [ ];
+      default = [];
       description = "List of partitions to add to the partition table";
     };
     _meta = lib.mkOption {
@@ -65,62 +70,65 @@
       readOnly = true;
       type = lib.types.functionTo diskoLib.jsonType;
       default = dev:
-        lib.foldr lib.recursiveUpdate { } (lib.imap
-          (index: partition:
-            lib.optionalAttrs (partition.content != null) (partition.content._meta dev)
+        lib.foldr lib.recursiveUpdate {} (lib.imap
+          (
+            index: partition:
+              lib.optionalAttrs (partition.content != null) (partition.content._meta dev)
           )
           config.partitions);
       description = "Metadata";
     };
     _create = diskoLib.mkCreateOption {
       inherit config options;
-      default = { dev }: ''
+      default = {dev}: ''
         parted -s ${dev} -- mklabel ${config.format}
         ${lib.concatStrings (lib.imap (index: partition: ''
-          ${lib.optionalString (config.format == "gpt") ''
-            parted -s ${dev} -- mkpart ${partition.name} ${diskoLib.maybeStr partition.fs-type} ${partition.start} ${partition.end}
-          ''}
-          ${lib.optionalString (config.format == "msdos") ''
-            parted -s ${dev} -- mkpart ${partition.part-type} ${diskoLib.maybeStr partition.fs-type} ${diskoLib.maybeStr partition.fs-type} ${partition.start} ${partition.end}
-          ''}
-          # ensure /dev/disk/by-path/..-partN exists before continuing
-          udevadm trigger --subsystem-match=block; udevadm settle
-          ${lib.optionalString partition.bootable ''
-            parted -s ${dev} -- set ${toString index} boot on
-          ''}
-          ${lib.concatMapStringsSep "" (flag: ''
-            parted -s ${dev} -- set ${toString index} ${flag} on
-          '') partition.flags}
-          # ensure further operations can detect new partitions
-          udevadm trigger --subsystem-match=block; udevadm settle
-          ${lib.optionalString (partition.content != null) (partition.content._create { dev = diskoLib.deviceNumbering dev index; })}
-        '') config.partitions)}
+            ${lib.optionalString (config.format == "gpt") ''
+              parted -s ${dev} -- mkpart ${partition.name} ${diskoLib.maybeStr partition.fs-type} ${partition.start} ${partition.end}
+            ''}
+            ${lib.optionalString (config.format == "msdos") ''
+              parted -s ${dev} -- mkpart ${partition.part-type} ${diskoLib.maybeStr partition.fs-type} ${diskoLib.maybeStr partition.fs-type} ${partition.start} ${partition.end}
+            ''}
+            # ensure /dev/disk/by-path/..-partN exists before continuing
+            udevadm trigger --subsystem-match=block; udevadm settle
+            ${lib.optionalString partition.bootable ''
+              parted -s ${dev} -- set ${toString index} boot on
+            ''}
+            ${lib.concatMapStringsSep "" (flag: ''
+                parted -s ${dev} -- set ${toString index} ${flag} on
+              '')
+              partition.flags}
+            # ensure further operations can detect new partitions
+            udevadm trigger --subsystem-match=block; udevadm settle
+            ${lib.optionalString (partition.content != null) (partition.content._create {dev = diskoLib.deviceNumbering dev index;})}
+          '')
+          config.partitions)}
       '';
     };
     _mount = diskoLib.mkMountOption {
       inherit config options;
-      default = { dev }:
-        let
-          partMounts = lib.foldr lib.recursiveUpdate { } (lib.imap
-            (index: partition:
-              lib.optionalAttrs (partition.content != null) (partition.content._mount { dev = diskoLib.deviceNumbering dev index; })
-            )
-            config.partitions);
-        in
-        {
-          dev = partMounts.dev or "";
-          fs = partMounts.fs or { };
-        };
+      default = {dev}: let
+        partMounts = lib.foldr lib.recursiveUpdate {} (lib.imap
+          (
+            index: partition:
+              lib.optionalAttrs (partition.content != null) (partition.content._mount {dev = diskoLib.deviceNumbering dev index;})
+          )
+          config.partitions);
+      in {
+        dev = partMounts.dev or "";
+        fs = partMounts.fs or {};
+      };
     };
     _config = lib.mkOption {
       internal = true;
       readOnly = true;
       default = dev:
         lib.imap
-          (index: partition:
+        (
+          index: partition:
             lib.optional (partition.content != null) (partition.content._config (diskoLib.deviceNumbering dev index))
-          )
-          config.partitions;
+        )
+        config.partitions;
       description = "NixOS configuration";
     };
     _pkgs = lib.mkOption {
@@ -128,9 +136,11 @@
       readOnly = true;
       type = lib.types.functionTo (lib.types.listOf lib.types.package);
       default = pkgs:
-        [ pkgs.parted pkgs.systemdMinimal ] ++ lib.flatten (map
-          (partition:
-            lib.optional (partition.content != null) (partition.content._pkgs pkgs)
+        [pkgs.parted pkgs.systemdMinimal]
+        ++ lib.flatten (map
+          (
+            partition:
+              lib.optional (partition.content != null) (partition.content._pkgs pkgs)
           )
           config.partitions);
       description = "Packages";
